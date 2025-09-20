7bee41779ffada13231d007fdba48de1f086379c
wjiajia <xsccdhmm114514@gmail.com>
2025-09-21 00:54:04 +0800

    release version 2

--- Action_people.h
+++ Action_people.h
@@ -6,8 +6,9 @@
 #include <Adafruit_PWMServoDriver.h>
 
 extern Adafruit_PWMServoDriver pwm;
+
 // 定义转动动作结构体
-struct RotationAction2 {
+struct RotationAction {
   uint8_t servoChannel : 4;  // 要控制的舵机通道 (0-15)
   uint8_t direction : 1;     // 方向：0顺时针，1逆时针
   uint8_t speed : 7;         // 速度：0-100%
@@ -15,7 +16,7 @@ struct RotationAction2 {
 };
 
 // 定义每个独立动作序列的状态结构体   
-struct ActionSequenceState2 {
+struct ActionSequenceState {
   RotationAction sequence[13]; // 该序列的动作数组
   int actionCount;             // 该序列的动作数量
   int currentActionIndex;      // 该序列当前执行的动作索引
@@ -25,18 +26,50 @@ struct ActionSequenceState2 {
 };
 
 // 声明全局变量
-extern const int MAX_CONCURRENT_SEQUENCES2;
-extern ActionSequenceState activeSequences2[];
-extern int activeSequenceCount2;
-extern bool servoChannelOccupied2[16];
+extern const int MAX_CONCURRENT_SEQUENCES;
+extern ActionSequenceState activeSequences[];
+extern int activeSequenceCount;
+extern bool servoChannelOccupied[16];
 
 // 声明函数
-void executeAction2(int sequenceIndex);
-int addActionSequence2(RotationAction actions[], int count);
-void startSequence2(int sequenceIndex);
-void stopSequence2(int sequenceIndex);
-void updateSequences2();
-
-uint16_t getPulseWidth2(unsigned long microseconds);
+void executeAction(int sequenceIndex);
+int addActionSequence(RotationAction actions[], int count);
+void startSequence(int sequenceIndex);
+void stopSequence(int sequenceIndex);
+void updateSequences();
+//人物动作
+int action_lvbu_attack_front();
+int action_lvbu_heal_front();
+int action_lvbu_skill1_front();
+int action_lvbu_skill1_back();
+int action_lvbu_skill2_front();
+int action_lvbu_skill2_back();
+int action_lvbu_die_front();
+int action_lvbu_die_back();
+int action_guanyu_attack_front();
+int action_guanyu_heal_front();
+int action_guanyu_skill1_front();
+int action_guanyu_skill1_back();
+int action_guanyu_skill2_front();
+int action_guanyu_skill2_back();
+int action_guanyu_die_front();
+int action_guanyu_die_back();
+int action_liubei_attack_front();
+int action_liubei_heal_front();
+int action_liubei_skill1_front();
+int action_liubei_skill1_back();
+int action_liubei_skill2_front();
+int action_liubei_skill2_back();
+int action_liubei_die_front();
+int action_liubei_die_back();
+int action_zhangfei_attack_front();
+int action_zhangfei_heal_front();
+int action_zhangfei_skill1_front();
+int action_zhangfei_skill1_back();
+int action_zhangfei_skill2_front();
+int action_zhangfei_skill2_back();
+int action_zhangfei_die_front();
+int action_zhangfei_die_back();
+uint16_t getPulseWidth(unsigned long microseconds);
 
 #endif
